1d48acd47f36a72f2b502bf3ef4c341fd3dec982
Alperen <alperensarisan@icloud.com>
2024-02-02 21:20:35 -0400

    Initial Commit

---
+++ MenuApp/MenuAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuAppApp.swift
+//  MenuApp
+//
+//  Created by Alperen Sarışan on 2.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct MenuAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
